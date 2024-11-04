cd8310e328cb7b53059df72a920523cea9a83bba
Helixjoe <joeljohn5112@gmail.com>
2024-11-04 20:54:24 +0530

    register page

--- ProtecTron - frontend/src/renderer/src/components/Header.jsx
+++ ProtecTron - frontend/src/renderer/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/images/Logo.png'
+import { Link } from 'react-router-dom'
 function Header() {
   return (
     <div className="w-full py-2 px-3">
@@ -7,9 +8,11 @@ function Header() {
           <img src={logo}></img>
           <h1 className="text-2xl font-bold text-blue-700">ProtecTron</h1>
         </div>
-        <button className="px-4 py-2  border-blue-700 text-blue-700 rounded-full border-2 hover:bg-blue-100">
-          Create an account
-        </button>
+        <Link to="/register">
+          <button className="px-4 py-2  border-blue-700 text-blue-700 rounded-full border-2 hover:bg-blue-100">
+            Create an account
+          </button>
+        </Link>
       </header>
     </div>
   )
--- ProtecTron - frontend/src/renderer/src/pages/Register.jsx
+++ ProtecTron - frontend/src/renderer/src/pages/Register.jsx
@@ -1,18 +1,26 @@
 // src/components/Login.js
 import google from '../assets/images/google-icon.png'
-function Login() {
+function Register() {
   return (
     <div className="flex items-center justify-center">
       <div className="w-full max-w-md p-8  bg-white border border-gray-200 rounded-lg shadow-md">
-        <h2 className="text-3xl font-bold text-center text-blue-700 py-6">Login</h2>
+        <h2 className="text-3xl font-bold text-center text-blue-700 ">Sign Up</h2>
 
         <form className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700">Full Name</label>
+            <input
+              type="text"
+              placeholder="Enter your Full Name"
+              className="w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded-md focus:ring-blue-700 focus:border-blue-700"
+            />
+          </div>
           {/* Username or Email Input */}
           <div>
             <label className="block text-sm font-medium text-gray-700">Email - ID</label>
             <input
               type="text"
-              placeholder="Enter your username or email"
+              placeholder="Enter your email"
               className="w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded-md focus:ring-blue-700 focus:border-blue-700"
             />
           </div>
@@ -25,9 +33,6 @@ function Login() {
               placeholder="Enter your password"
               className="w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded-md focus:ring-blue-700 focus:border-blue-700"
             />
-            <p className="mt-1 text-xs text-right text-blue-700 cursor-pointer hover:underline">
-              Forgot your password?
-            </p>
           </div>
 
           {/* Login Button */}
@@ -35,7 +40,7 @@ function Login() {
             type="submit"
             className="w-full py-2 mt-4 text-sm font-semibold text-white bg-blue-700 rounded-md hover:bg-blue-800"
           >
-            Login
+            Sign up
           </button>
 
           {/* OR Separator */}
@@ -55,17 +60,10 @@ function Login() {
             />
             Continue with Google
           </button>
-
-          {/* Create an Account Link */}
-          <p className="mt-4 text-center text-sm text-blue-700">
-            <a href="#" className="hover:underline">
-              Create an account
-            </a>
-          </p>
         </form>
       </div>
     </div>
   )
 }
 
-export default Login
+export default Register
